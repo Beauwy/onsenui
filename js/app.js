ad4bb4fa39bdbdcab0dd8fd3de95416010144629
Beauwy <38246139+Beauwy@users.noreply.github.com>
2019-10-22 20:59:04 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,40 +1,101 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDpGGVZib9xCafbvGhoEj1T1-jJ0ptkab8",
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDpGGVZib9xCafbvGhoEj1T1-jJ0ptkab8",
     authDomain: "foodpj-9550d.firebaseapp.com",
     databaseURL: "https://foodpj-9550d.firebaseio.com",
     projectId: "foodpj-9550d",
     storageBucket: "foodpj-9550d.appspot.com",
     messagingSenderId: "603895277613",
     appId: "1:603895277613:web:e330f1b4fe8ad86b0870a4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  //Use firestore
 var db = firebase.firestore();
 
-window.fn = {};
+document.addEventListener('init', function (event) {
+  var page = event.target;
 
-window.fn.open = function() {
-  var menu = document.getElementById('menu');
-  menu.open();
-};
 
-window.fn.load = function(page) {
-  var content = document.getElementById('content');
-  var menu = document.getElementById('menu');
-  content.load(page)
-    .then(menu.close.bind(menu));
-};
+  if (page.id === 'homePage') {
+    console.log("homePage");
 
-var login = function() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-  
-    if (username === 'bob' && password === 'secret') {
-      ons.notification.alert('Congratulations!');
-    } else {
-      ons.notification.alert('Incorrect username or password.');
-    }
-  };
+    $("#thaibtn").click(function () {
+      localStorage.setItem("selectedCategory", "thai");
+      $("#content")[0].load("category.html");
+    });
+
+    $("#drinkbtn").click(function () {
+      localStorage.setItem("selectedCategory", "drink");
+      $("#content")[0].load("category.html");
+    });
+
+    $("#menubtn").click(function () {
+      $("#sidemenu")[0].open();
+    });
+
+    $("carousel").empty();
+    db.collection("restaurant").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        var item = `<ons-carousel-item modifier="nodivider" id="item${doc.data().rest_id}" class="recomended_item" onclick="">
+            <img src="${doc.data().rest_url}" width="120" height="120"></div>
+            <div class="recomended_item_title" id="item1_${doc.data().rest_id}">${doc.data().rest_name}</div>
+        </ons-carousel-item>`
+        $("#carousel").append(item);
+      });
+    });
+  }
+
+  if (page.id === 'menuPage') {
+    console.log("menuPage");
+
+    $("#login").click(function () {
+      $("#content")[0].load("login.html");
+      $("#sidemenu")[0].close();
+    });
+
+    $("#home").click(function () {
+      $("#content")[0].load("home.html");
+      $("#sidemenu")[0].close();
+    });
+  }
+
+  if (page.id === 'categoryPage') {
+    var category = localStorage.getItem("selectedCategory");
+    console.log("categoryPage:" + category);
+
+    $("#header").html(category);
+
+    $("#menubtn").click(function () {
+      $("#sidemenu")[0].open();
+    });
+
+    $("#list").empty();
+    db.collection("recommended").where("category", "==", category).get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        var item = `<ons-row class="category">
+                <ons-col modifier="nodivider">
+                    <div class="category_header" style="background-image: url('${doc.data().photoUrl}')">
+                        <figure class="category_thumbnail" id="thaibtn">
+                            <div class="category_title" id="Category_1_name">${doc.data().name}</div>
+                        </figure>
+                    </div>
+                </ons-col>
+         </ons-row>`
+        $("#list").append(item);
+        console.log(doc.data().name);
+        
+      });
+    });
+
+  }
+
+  if (page.id === 'loginPage') {
+    console.log("loginPage");
+
+    $("#backhomebtn").click(function () {
+      $("#content")[0].load("home.html");
+    });
+  }
+});
